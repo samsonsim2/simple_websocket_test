6d8633083aeb3bf0dadb09b86ef8ca955617ad36
Samson Sim <samsonsim2@gmail.com>
2024-10-01 00:54:14 +0800

    send message to client

--- index.js
+++ index.js
@@ -4,4 +4,14 @@ const wss = new WebSocket.Server({port:8080});
 
 wss.on("connection",ws=>{
     console.log("new client connected");
+
+    ws.on("message", data=>{
+        console.log(`Client has sent us : ${data}`);
+        ws.send(data.toUpperCase());
+
+    })
+
+    ws.on("close",()=>{
+        console.log("client has disconnected");
+    })
 })
