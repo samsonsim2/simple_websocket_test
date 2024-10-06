77afdcb9f494a46d5aeadc69e1cf495ab713312b
Samson Sim <samsonsim2@gmail.com>
2024-10-07 01:15:36 +0800

    connect to all clients

--- index.js
+++ index.js
@@ -2,19 +2,34 @@ const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({port:8080});
 
-wss.on("connection",ws=>{
-    console.log("new client connected");
+// wss.on("connection",ws=>{
+//     console.log("new client connected");
 
+//     ws.on("message", data=>{
+        
+//         console.log(`Client has sent us : ${data}`);
+//         // const jsonObject = {
+//         //     frequency:5.0
+//         //   };
+//         ws.send(JSON.stringify(data));
+
+//     })
+
+//     ws.on("close",()=>{
+//         console.log("client has disconnected");
+//     })
+// })
+
+wss.on('connection', function connection(ws) {
     ws.on("message", data=>{
         console.log(`Client has sent us : ${data}`);
-        // const jsonObject = {
-        //     frequency:5.0
-        //   };
-        ws.send(JSON.stringify(data));
-
-    })
-
-    ws.on("close",()=>{
-        console.log("client has disconnected");
-    })
-})
+  
+      // Broadcast to all clients
+      wss.clients.forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN) {
+           client.send(JSON.stringify(data));
+      
+        }
+      });
+    });
+  });
