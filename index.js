b39a42e57f5edb157b4c4a050fabf1c9a42be2ab
Samson Sim <samsonsim2@gmail.com>
2024-10-19 14:31:36 +0800

    store data

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const WebSocket = require('ws');
+const WebSocket = require("ws");
 
-const wss = new WebSocket.Server({port:8080});
+const wss = new WebSocket.Server({ port: 8080 });
 
 // wss.on("connection",ws=>{
 //     console.log("new client connected");
 
 //     ws.on("message", data=>{
-        
+
 //         console.log(`Client has sent us : ${data}`);
 //         // const jsonObject = {
 //         //     frequency:5.0
@@ -20,16 +20,34 @@ const wss = new WebSocket.Server({port:8080});
 //     })
 // })
 
-wss.on('connection', function connection(ws) {
-    ws.on("message", data=>{
-        console.log(`Client has sent us : ${data}`);
-  
-      // Broadcast to all clients
-      wss.clients.forEach(function each(client) {
-        if (client.readyState === WebSocket.OPEN) {
-           client.send(JSON.stringify(data));
-      
-        }
-      });
+wss.on("connection", function connection(ws) {
+  let jsonData = {
+    time: 0.0,
+    minStep: 0.1,
+    maxStep: 0.6,
+    frequency: 1.0,
+    color1: new THREE.Color(0xffffff),
+    color2: new THREE.Color(0x000000),
+    speed: 1,
+    invert: 0.0,
+    step: 0.0,
+    noiseOption: 0.0,
+  };
+
+  client.send(JSON.stringify(jsonData));
+
+
+
+  ws.on("message", (data) => {
+    console.log(`Client has sent us : ${data}`);
+
+    jsonData = data
+
+    // Broadcast to all clients
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(jsonData));
+      }
     });
   });
+});
